2dbd7222f5b70bb0d944eca72fb5cad1c97d6d42
Deva Raja <125891830+deltadv@users.noreply.github.com>
2023-04-13 12:49:47 +0700

    Mengubah comments pada fungsi calculateTax()
    Kode telah diubah dengan menambahkan komentar yang jelas dan berguna pada fungsi calculateTax()

--- TaxCalculator-SCD/TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator-SCD/TaxCalculator/src/lib/TaxFunction.java
@@ -39,14 +39,13 @@ public class TaxFunction {
   }
 
   /**
-   * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
+   * Menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
    * 
-   * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-   * 
-   * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-   * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-   * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-   * 
+   * @param monthlyIncome Penghasilan bulanan pegawai (gaji dan pemasukan bulanan lainnya).
+   * @param numberOfMonthWorking Jumlah bulan yang dikerjakan pegawai dalam setahun.
+   * @param deductible Pengurang dari penghasilan bruto.
+   * @param employeeFamily Keluarga pegawai (status perkawinan dan jumlah anak).
+   * @return Jumlah pajak penghasilan yang harus dibayarkan setahun.
    */
   public static int calculateTax(MonthlyIncome monthlyIncome, int numberOfMonthWorking, int deductible, EmployeeFamily employeeFamily) {
 
